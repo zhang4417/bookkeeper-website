6c650f45475f92e40b408c4c2520026fe9d58e87
zhang4417 <1804815164@qq.com>
2020-12-01 16:45:36 +0800

    deploy

---
+++ precache-manifest.fc1b4741f546d1fcbd47bd6a13a8ee01.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d97e05c7775fb515dcf",
+    "url": "/bookkeeper-website/css/app.7a111d50.css"
+  },
+  {
+    "revision": "e0c28cd94ede4f1277429aad6aba0a4a",
+    "url": "/bookkeeper-website/index.html"
+  },
+  {
+    "revision": "5d97e05c7775fb515dcf",
+    "url": "/bookkeeper-website/js/app.cd620567.js"
+  },
+  {
+    "revision": "c7709e51c62a4da70644",
+    "url": "/bookkeeper-website/js/chunk-vendors.c3a13ec7.js"
+  },
+  {
+    "revision": "34081813f67ceece5d9fce8641e3e8a2",
+    "url": "/bookkeeper-website/manifest.json"
+  },
+  {
+    "revision": "922bd50b5f067966d58fe59d44b85721",
+    "url": "/bookkeeper-website/qrCode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bookkeeper-website/robots.txt"
+  }
+]);
